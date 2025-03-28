3e4eeef8479ba0a4a2bb977931248ca4728dbba6
bhatt04 <meghabhatt0403@gmail.com>
2025-03-28 17:21:02 +0530

    added audio

--- index.js
+++ index.js
@@ -1,62 +1,15 @@
-// const startE1 = document.getElementById("start");
-// const stopE1 = document.getElementById("stop");
-// const resetE1 = document.getElementById("reset");
-// const timerE1 = document.getElementById("timer");
-
-// let interval;
-// let timeleft = 1500;
-
-// function updateTimer(){
-//     let minutes = Math.floor(timeleft / 60);
-//     let seconds = timeleft % 60;
-
-//     let formattedTime = `${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
-
-//     timerE1.innerText = formattedTime;
-// }
-// function startTimer(){  //START
-//     // console.log(("started"));
-//     interval = setInterval(()=>{
-//     timeleft--;
-//     // console.log("hello");
-//     updateTimer();
-//     if(timeleft === 0){
-//         clearInterval(interval);
-//         alert("Time's up!");
-//         timeleft=1500;
-//       }
-//     }, 1000)
-// }
-
-
-// function stopTimer(){      //STOP
-//     // console.log(("stop"));
-//     clearInterval(interval);    
-// }
-
-
-// function resetTimer(){  //RESET
-//     // console.log(("reset"));
-//     clearInterval(interval);
-//     timeleft = 1500;
-//     updateTimer();
-    
-// }
-
-// startE1.addEventListener("click" , startTimer)
-// stopE1.addEventListener("click" , stopTimer)
-// resetE1.addEventListener("click" , resetTimer)
-
-
-
-
-
 
 const startE1 = document.getElementById("start");
 const stopE1 = document.getElementById("stop");
 const resetE1 = document.getElementById("reset");
 const timerE1 = document.getElementById("timer");
 const titleE1 = document.getElementById("title");
+
+//popup
+const popup = document.getElementById("popup");
+const closePopup = document.getElementById("close-popup");
+const alarmSound = document.getElementById("alarm-sound");
+popup.style.display = "none";
 
 let interval;
 let timeleft = 1500;
@@ -69,13 +22,8 @@ function updateTimer() {
 }
 
 function startTimer() {
-    // Hide the title
     titleE1.style.opacity = "0";
-    
-    // Increase timer font size
     timerE1.classList.add("fullscreen");
-
-    // Show Stop and Reset buttons
     stopE1.classList.remove("hidden");
     resetE1.classList.remove("hidden");
 
@@ -83,9 +31,13 @@ function startTimer() {
     interval = setInterval(() => {
         timeleft--;
         updateTimer();
+
         if (timeleft === 0) {
             clearInterval(interval);
-            alert("Time's up!");
+            // alert("Time's up!");
+            popup.style.display = "flex";
+            // alarmSound.play();
+            playAlarmSound();
             timeleft = 1500;
         }
     }, 1000);
@@ -106,7 +58,25 @@ stopE1.addEventListener("click", stopTimer);
 resetE1.addEventListener("click", resetTimer);
 
 
+function playAlarmSound() {
+    alarmSound.play().catch(error => {
+        console.log("Audio playback failed:", error);
+    });
+}
 
+function stopAlarmSound() {
+    alarmSound.pause(); 
+    alarmSound.currentTime = 0; 
+}
+
+
+closePopup.addEventListener("click", () => {
+    popup.style.display = "none";
+    stopAlarmSound();
+});
+
+
+//toogle 
 const inputE1= document.querySelector(".input");
 const bodyE1 = document.querySelector("body");
 
@@ -116,7 +86,7 @@ function updateBody(){
     if(inputE1.checked){
         bodyE1.style.background="rgb(5, 19, 75)";
     }else{
-        bodyE1.style.background="rgb(111, 97, 234) ";
+        bodyE1.style.background="#6f61ea ";
     }
 }
 
